b9db5d202e11b108e7524db19548ddefccd8d906
enmanuel <enmanuel.josue.torres13@gmail.com>
2024-12-10 23:50:05 -0400

    feet: change in view  contactos and components UI

--- src/utils/aliados.js
+++ src/utils/aliados.js
@@ -7,63 +7,63 @@ import { Pagination, Mousewheel, Autoplay } from "swiper/modules";
 gsap.registerPlugin(ScrollTrigger);
 
 if (window.innerWidth > 1024) {
-    gsap.set(".animated-image", { scale: 1.5 });
+  gsap.set(".animated-image", { scale: 1.45 });
 
-    // Animación de escala
-    gsap.to(".animated-image", {
-        scale: 0.6, // Tamaño original
-        scrollTrigger: {
-            trigger: ".h-screen", // Elemento que activa el scroll
-            start: "top top", // Inicia cuando la parte superior del elemento coincide con la parte superior de la ventana
-            end: "bottom top", // Termina cuando la parte inferior del elemento alcanza la parte superior de la ventana
-            scrub: true, // Permite un desplazamiento suave
-            pin: true, // Fija el elemento durante el scroll
-        },
-        ease: "power1.out", // Efecto de suavizado
-    });
+  // Animación de escala
+  gsap.to(".animated-image", {
+    scale: 0.6, // Tamaño original
+    scrollTrigger: {
+      trigger: ".h-screen", // Elemento que activa el scroll
+      start: "top top", // Inicia cuando la parte superior del elemento coincide con la parte superior de la ventana
+      end: "bottom top", // Termina cuando la parte inferior del elemento alcanza la parte superior de la ventana
+      scrub: true, // Permite un desplazamiento suave
+      pin: true, // Fija el elemento durante el scroll
+    },
+    ease: "power1.out", // Efecto de suavizado
+  });
 }
 
 var swiper = new Swiper(".default-carousel", {
-    modules: [Pagination, Mousewheel, Autoplay], // Incluye Mousewheel
-    loop: true,
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-    },
-    grabCursor: true,
-    mousewheel: {
-        forceToAxis: true, // Restringe al eje principal
-        sensitivity: 0.5, // Reduce la sensibilidad del desplazamiento
-        thresholdDelta: 20, // Ignora pequeños desplazamientos
-    },
-    touchRatio: 0.5,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 7000, // Retraso entre transiciones (en ms)
-        disableOnInteraction: false, // Continúa el autoplay después de la interacción del usuario
-    },
+  modules: [Pagination, Mousewheel, Autoplay], // Incluye Mousewheel
+  loop: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  grabCursor: true,
+  mousewheel: {
+    forceToAxis: true, // Restringe al eje principal
+    sensitivity: 0.5, // Reduce la sensibilidad del desplazamiento
+    thresholdDelta: 20, // Ignora pequeños desplazamientos
+  },
+  touchRatio: 0.5,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 7000, // Retraso entre transiciones (en ms)
+    disableOnInteraction: false, // Continúa el autoplay después de la interacción del usuario
+  },
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const images = document.querySelectorAll('.img');
-  const sections = document.querySelectorAll('[data-index]');
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll(".img");
+  const sections = document.querySelectorAll("[data-index]");
 
   // Comprobar si el ancho de pantalla es mayor a 'md' (768px)
-  const mediaQuery = window.matchMedia('(min-width: 768px)');
+  const mediaQuery = window.matchMedia("(min-width: 768px)");
 
   // Array de URLs para precargar
   const imageUrls = [
-    '/Aliados/Semwesven-01.webp',
-    '/Aliados/Semwesven-02.webp',
-    '/Aliados/Semwesven-03.webp'
+    "/Aliados/Semwesven-01.webp",
+    "/Aliados/Semwesven-02.webp",
+    "/Aliados/Semwesven-03.webp",
   ];
 
   // Función para precargar imágenes
   const preloadImages = (urls) => {
-    urls.forEach(url => {
+    urls.forEach((url) => {
       const img = new Image();
       img.src = url;
     });
@@ -79,12 +79,12 @@ document.addEventListener('DOMContentLoaded', () => {
         (entries) => {
           entries.forEach((entry) => {
             if (entry.isIntersecting) {
-              const index = entry.target.getAttribute('data-index');
+              const index = entry.target.getAttribute("data-index");
 
               // Mostrar la imagen correspondiente
               images.forEach((img, i) => {
-                img.classList.toggle('visible', i == index);
-                img.classList.toggle('hidden', i != index);
+                img.classList.toggle("visible", i == index);
+                img.classList.toggle("hidden", i != index);
               });
             }
           });
@@ -97,9 +97,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  mediaQuery.addEventListener('change', handleMediaQuery);
+  mediaQuery.addEventListener("change", handleMediaQuery);
 
   // Llamar a la función inicialmente
   handleMediaQuery(mediaQuery);
 });
-
