09dc400d08b7229ea418aaf783234c8c184cc181
enmanuel <enmanuel.josue.torres13@gmail.com>
2024-12-15 22:45:41 -0400

    feet: change test server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
   integrations: [tailwind()],
   vite: {
     server: {
