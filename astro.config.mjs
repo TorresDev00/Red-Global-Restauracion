1fd72ac1036b613b27fdc0c28aec73aecfc32333
enmanuel <enmanuel.josue.torres13@gmail.com>
2024-12-15 21:09:30 -0400

    feet: change config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,22 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   integrations: [tailwind()],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true, // Útil si hay problemas de caché local
+        interval: 100,    // Ajusta el intervalo de actualización
+      },
+      hmr: {
+        overlay: true, // Muestra errores en la consola del navegador
+      },
+    },
+    optimizeDeps: {
+      include: ['sweetalert2'], // Optimiza manualmente ciertas dependencias
+    },
+  },
 });
